bdc1e9ed83226890f82e254ebb33bd39a508f434
Dominik Bullo <dominik.bullo@gmail.com>
2022-05-16 23:04:08 +0200

    moved store from index.js to src/store/

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import reducer from "./reducers";
-import middleware from "./middleware";
-
-export const store = createStore(reducer, middleware);
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
